49077ed69f3420ae4acae5f8b9ea3436ff799c85
mohamedhossam <wikohossam35@gmail.com>
2022-09-17 14:56:15 +0200

    event loop examples

---
+++ event_loop_examples/3-setInterval.js
@@ -0,0 +1,9 @@
+setInterval(() => {
+  console.log("Hello Wiko");
+}, 2000);
+
+console.log("I will run first");
+
+/*
+    process still alive unless kill process  control + c , unexpected error
+*/
